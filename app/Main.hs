2c30d2b13502f5dfb52c6e06b503166bf937452a
Franco Bulgarelli <franco@mumuki.org>
2017-12-06 21:26:50 -0300

    Adding main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Text.Reparser
+import System.Exit
+import Data.Maybe
+
 main :: IO ()
-main = return ()
+main = do
+  body <- getContents
+  case (reparse body) of
+    (Just error) -> putStrLn (show error) >> exitWith (ExitFailure 1)
+    _            -> exitWith ExitSuccess
+
